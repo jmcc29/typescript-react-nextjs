6ff5a173e7d1249dfc7bfcb9afec171f46c7b5eb
José <jcuencacoronado@gmail.com>
2025-04-03 16:39:36 -0400

    Tercer commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-function Button(){
-  return (
-    <button>Click</button>
-  )
+"use client"
+import React, {JSX} from "react"
+type ButtonProps = {
+  //onClick: (text: string) => void;
+  //children: JSX.Element | JSX.Element[] | string;
 }
-function Page(){
+function Button({}: ButtonProps) {
+  return <button /*onClick={() => onClick("Hello World")}*/>test</button>
+}
+//const Button = ({ text }: ButtonProps) => (<button>{text}</button>)
+//const Button: React.FC<ButtonProps> = ({ text, subtitle }) => (<button>{text}</button>)
+
+function Page() {
   return (
     <div>
-      <Button />
+      <Button/>
     </div>
   )
 }
