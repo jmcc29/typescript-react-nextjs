b859d3877a7e8a525e41d55ac1d2dd1b2f4255be
José <jcuencacoronado@gmail.com>
2025-04-07 15:08:15 -0400

    Intersections y extends

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
 "use client";
 import {ComponentPropsWithoutRef} from "react";
-type ButtonProps = ComponentPropsWithoutRef<"button">
-function Button({onClick, ...rest}: ButtonProps) {
-  const handleClick = () => {
-    if (onClick) {
-      alert('Button clicked!')
-    }
-  };
-  return <button onClick={handleClick} {...rest}>Button</button>;
+// type Props = ComponentPropsWithoutRef<"button"> 
+// type ButtonProps = Props & {
+//   dark?: boolean;
+//   variant?: "primary" | "secondary";
+// }
+interface Props extends ComponentPropsWithoutRef<"button"> {
+  dark?: boolean;
+  variant?: "primary" | "secondary";
+}
+interface ButtonProps extends Props {}
+function Button({dark, variant, children}: ButtonProps) {
+  return <button>Button</button>;
 }
 function Page() {
   return (
     <div>
-      <Button 
-        onClick={() => alert("Hello World!")}
-        style={{
-          color: "white",
-          backgroundColor: "blue",
-        }}
-        >
-        Click me
+      <Button>
+        asdf
       </Button>
     </div>
   );
