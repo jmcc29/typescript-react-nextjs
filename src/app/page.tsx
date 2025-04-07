6ad88657b15face12fc2316d5f79dde93703bee6
José <jcuencacoronado@gmail.com>
2025-04-07 13:47:39 -0400

    Default props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 "use client"
-import React, {JSX, use, useState, Dispatch} from "react"
 type ButtonProps = {
-  setCount: Dispatch<React.SetStateAction<number>>
-  //onClick: (text: string) => void;
-  //children: JSX.Element | JSX.Element[] | string;
+  title?: string
 }
-function Button({setCount}: ButtonProps) {
-  return <button onClick={() => setCount(10)}
-  /*onClick={() => onClick("Hello World")}*/>test</button>
+function Button({title="Text Here!"}: ButtonProps) {
+  return <button>{title}</button>
 }
-//const Button = ({ text }: ButtonProps) => (<button>{text}</button>)
-//const Button: React.FC<ButtonProps> = ({ text, subtitle }) => (<button>{text}</button>)
-
 function Page() {
-  const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <Button setCount={setCount}/>
+      <Button/>
     </div>
   )
 }
