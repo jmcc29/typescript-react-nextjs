049de2ebb1cb88244b03a7fcc3f58c8b3ce0c840
José <jcuencacoronado@gmail.com>
2025-04-07 13:40:54 -0400

    Uso de setters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 "use client"
-import React, {JSX} from "react"
+import React, {JSX, use, useState, Dispatch} from "react"
 type ButtonProps = {
+  setCount: Dispatch<React.SetStateAction<number>>
   //onClick: (text: string) => void;
   //children: JSX.Element | JSX.Element[] | string;
 }
-function Button({}: ButtonProps) {
-  return <button /*onClick={() => onClick("Hello World")}*/>test</button>
+function Button({setCount}: ButtonProps) {
+  return <button onClick={() => setCount(10)}
+  /*onClick={() => onClick("Hello World")}*/>test</button>
 }
 //const Button = ({ text }: ButtonProps) => (<button>{text}</button>)
 //const Button: React.FC<ButtonProps> = ({ text, subtitle }) => (<button>{text}</button>)
 
 function Page() {
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <Button/>
+      <h1>Count: {count}</h1>
+      <Button setCount={setCount}/>
     </div>
   )
 }
